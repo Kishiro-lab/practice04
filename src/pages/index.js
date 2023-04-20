75db3d43ff25f056fc6c0847495a76087024e6f7
kishiro <the.yuhei@gmail.com>
2023-04-20 11:39:34 +0900

    basenameを削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/practice04">
+    <BrowserRouter>
       <HelmetProvider>
         <App />
       </HelmetProvider>
