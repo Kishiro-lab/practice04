f0bae73518435ea0bccbcf18bc86d364e984663d
kishiro <the.yuhei@gmail.com>
2023-04-20 10:14:09 +0900

    basename変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/practice04">
       <HelmetProvider>
         <App />
       </HelmetProvider>
